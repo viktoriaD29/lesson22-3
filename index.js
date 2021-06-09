b6faa61eeae91040464572f3e25dbcf3dab1fea1
Viktoria <doduchenko29@gmail.com>
2021-06-09 14:52:16 +0300

    add

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const checkElem = document.querySelector('.task-status');
 function getCheck () {
-  const checkboxElem = document.querySelector('checked');
+  const checkboxElem = document.querySelector('input[type=checkbox]');
   if (checkboxElem.value) {
     console.log(true)
   } else {
